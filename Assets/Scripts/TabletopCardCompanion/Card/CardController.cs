bf46aa3ac33139e48d4c084c14697db316631085
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-09-23 18:33:51 -0700

    Refactor Cards and add way to flip them over

---
+++ Assets/Scripts/TabletopCardCompanion/Card/CardController.cs
@@ -0,0 +1,50 @@
+﻿using TouchScript.Gestures;
+using UnityEngine;
+
+namespace TabletopCardCompanion
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(TapGesture))]
+    public class CardController : MonoBehaviour
+    {
+        [SerializeField] private TwoSidedImage twoSidedImage;
+
+        // Components
+        private BoxCollider2D boxCollider;
+        private TapGesture tapGesture;
+
+        private void Awake()
+        {
+            boxCollider = GetComponent<BoxCollider2D>();
+            tapGesture = GetComponent<TapGesture>();
+        }
+
+        private void OnEnable()
+        {
+            tapGesture.Tapped += TappedHandler;
+            twoSidedImage.SizeChanged += SizeChangedHandler;
+        }
+
+        private void OnDisable()
+        {
+            tapGesture.Tapped -= TappedHandler;
+            twoSidedImage.SizeChanged -= SizeChangedHandler;
+        }
+
+        /// <summary>
+        /// Flip the card over when tapped.
+        /// </summary>
+        private void TappedHandler(object sender, System.EventArgs e)
+        {
+            twoSidedImage.Flip();
+        }
+
+        /// <summary>
+        /// Resize the BoxCollider2D size to match the new image.
+        /// </summary>
+        private void SizeChangedHandler(object sender, Bounds bounds)
+        {
+            boxCollider.size = bounds.size;
+        }
+    }
+}
---
+++ Assets/Scripts/TabletopCardCompanion/TwoSidedImage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TabletopCardCompanion
+{
+    /// <summary>
+    /// Represents a two-sided image (i.e. a playing card or board tile).
+    /// </summary>
+    [RequireComponent(typeof(RectTransform))]
+    [RequireComponent(typeof(Image))]
+    public class TwoSidedImage : MonoBehaviour
+    {
+        public event EventHandler<Bounds> SizeChanged;
+
+        [SerializeField] private Sprite front;
+        [SerializeField] private Sprite back;
+        private bool isFront = true;
+
+        // Components
+        private RectTransform rectTransform;
+        private Image image;
+
+        private void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            image = GetComponent<Image>();
+        }
+
+        private void Start()
+        {
+            image.sprite = front;
+            image.SetNativeSize();
+            SizeChanged?.Invoke(this, front.bounds);
+        }
+
+        /// <summary>
+        /// "Flips" over the object in a way that looks physical.
+        /// </summary>
+        public void Flip()
+        {
+            var newSprite = isFront ? back : front;
+            isFront = !isFront;
+
+            // Lerp rectTransform.x to zero.
+            // Image.sprite = newSprite
+            // var newWidth = newSprite.width
+            // Lerp rectTransform.x to newWidth.
+
+            // Temporary hack:
+            image.sprite = newSprite;
+            image.SetNativeSize();
+            SizeChanged?.Invoke(this, newSprite.bounds);
+        }
+    }
+}
